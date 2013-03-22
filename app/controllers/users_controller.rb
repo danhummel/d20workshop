f929a835f0fff6e372ace9a91247dcd82f73ab63
Daniel Hummel <dhummel8@gmail.com>
2013-03-22 16:27:50 -0700

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,79 @@
+class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+    before_filter :correct_user,   only: [:edit, :update]
+    before_filter :admin_user,     only: :destroy
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		if signed_in?
+			signed_in_redirect
+		else
+			@user = User.new
+		end
+	end
+
+	def create
+		if signed_in?
+			signed_in_redirect
+		else
+			@user = User.new(params[:user])			
+			if @user.save
+				sign_in @user
+				flash[:success] = "Welcome to the Sample App!"
+				redirect_to @user			
+			else
+				render 'new'
+			end
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+	def destroy
+		flash[:success] = "#{User.find(params[:id]).destroy.name}'s profile destroyed."
+		redirect_to users_url
+	end
+	
+	private
+
+		def signed_in_redirect
+			flash[:error] = "Please logout before creating a new account."
+			redirect_to root_path
+		end
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, notice: "Please sign in." unless signed_in?
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D20workshop::Application.config.secret_token = '8b18a61fed88c1dee9f0f729b4bedcfdf2a87d0c4adf3952306b857012cdbb0544e8bc50af59b3f8e195e41483481b29d1adbe6b73ccb3a06802577cc8ffe7bf'
